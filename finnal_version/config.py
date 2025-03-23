58a0d26e63aaf0fcff0099a68e6edb2082677748
Amin <taaa1799@gmail.com>
2025-03-23 22:03:42 +0330

    another part of changes

--- finnal_version/config.py
+++ finnal_version/config.py
@@ -11,6 +11,18 @@ PLAYER1_Y = random.randint(1,1080)
 PLAYER2_X = random.randint(1,1920)
 PLAYER2_Y = random.randint(1,1080)
 
+PLAYER1_BULLETS = 10
+PLAYER2_BULLETS = 10
+
+
+PLAYER1_SHOTS = []
+PLAYER2_SHOTS = []
+
+
+
+show_text_p1 = True 
+show_text_p2 = True 
+
 speed = 3
 
 
--- finnal_version/main.py
+++ finnal_version/main.py
@@ -19,30 +19,82 @@ time_left_p2 = initial_time_p2
 font = pygame.font.Font(None, 36) 
 clock = pygame.time.Clock()
 last_update = pygame.time.get_ticks()
+blink_timer = pygame.time.get_ticks()
+
+
+# background music setting
+pygame.mixer.init() 
+pygame.mixer.music.load("finnal_version\media\Background.mp3") 
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1)  
+
+# shot soundeffect setting
+shoot_sound = pygame.mixer.Sound("finnal_version\media\Headshot.wav")  
+shoot_sound.set_volume(0.3) 
 
 
 
 
 
-
+blink1 = False
+blink2 = False
 while running:
     clock.tick(72)
     win.blit(background, (0, 0))
     current_time = pygame.time.get_ticks()
     if current_time - last_update >= 1000: 
         if time_left_p1 > 0:
-            time_left_p1 -= 1  
+                time_left_p1 -= 1
         if time_left_p2 > 0:
             time_left_p2 -= 1
         last_update = current_time 
+        blink = True
+    else:
+        blink = False
+
+    #timer blink 
+    if time_left_p1 < 30:
+        color_p1 = (255, 0, 0) 
+        if current_time - blink_timer >= 500: 
+            show_text_p1 = not show_text_p1
+            blink_timer = current_time
+    else:
+        color_p1 = (255, 255, 255)  
+        show_text_p1 = True  
+
+    if time_left_p2 < 30:
+        color_p2 = (255, 0, 0) 
+        if current_time - blink_timer >= 500:
+            show_text_p2 = not show_text_p2
+            blink_timer = current_time
+    else:
+        color_p2 = (255, 255, 255)  
+        show_text_p2 = True
 
 
-    timer_text_p1 = font.render(f"Player 1 Time: {time_left_p1}s", True, (255, 255, 255))
-    win.blit(timer_text_p1, (20, 20))
 
 
-    timer_text_p2 = font.render(f"Player 2 Time: {time_left_p2}s", True, (255, 255, 255))
-    win.blit(timer_text_p2, (WIDTH - timer_text_p2.get_width() - 20, 20))
+
+
+    # Texts
+
+    if show_text_p1:
+        timer_text_p1 = font.render(f"Player 1 Time: {time_left_p1}s", True, color_p1)
+        win.blit(timer_text_p1, (20, 20))
+
+    if show_text_p2:
+        timer_text_p2 = font.render(f"Player 2 Time: {time_left_p2}s", True, color_p2)
+        win.blit(timer_text_p2, (WIDTH - timer_text_p2.get_width() - 20, 20))
+
+
+    timer_text_p1 = font.render(f"Player 1 Bullets: {PLAYER1_BULLETS}", True, (255, 255, 255))
+    win.blit(timer_text_p1, (20, 42))
+    timer_text_p1 = font.render(f"Player 2 Bullets: {PLAYER2_BULLETS}", True, (255, 255, 255))
+    win.blit(timer_text_p1, (1685, 42))
+    #-----------------------------------------------------
+
+
+
 
 
     keys = pygame.key.get_pressed()
@@ -93,6 +145,32 @@ while running:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_SPACE:
+                if PLAYER2_BULLETS != 0:
+                    PLAYER2_SHOTS.append((PLAYER2_X , PLAYER2_Y))
+                    PLAYER2_BULLETS -= 1
+                    shoot_sound.play()
+                else:
+                    pass
+            if event.key == K_RSHIFT:
+                if PLAYER1_BULLETS != 0:
+                    PLAYER1_SHOTS.append((PLAYER1_X , PLAYER1_Y))
+                    PLAYER1_BULLETS -= 1
+                    shoot_sound.play()
+                else:
+                    pass
+    
+
+
+
+
     pygame.draw.circle(win, (250,250,250), (PLAYER1_X,PLAYER1_Y), 5)
     pygame.draw.circle(win, (0,0,0), (PLAYER2_X,PLAYER2_Y), 5)
+
+    for shot in PLAYER2_SHOTS:
+        pygame.draw.circle(win, (0,0,0), (shot[0],shot[1]), 3)
+    for shot in PLAYER1_SHOTS:
+        pygame.draw.circle(win, (250,250,250), (shot[0],shot[1]), 3)
+
     pygame.display.update()
